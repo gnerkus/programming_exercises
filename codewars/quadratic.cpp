67ae44fb44a50c783c3d9fb6a541482ae931601c
gnerkus <ifeanyioraelosi@gmail.com>
2020-07-05 21:52:22 +0200

    Codewars: Quadratic

---
+++ codewars/quadratic.cpp
@@ -0,0 +1,16 @@
+#include <vector>
+using namespace std;
+// My solution
+vector<int> quadratic(int a,int b){
+// Answer in vector
+  vector<int> coefficients;
+  coefficients.push_back(1);
+  coefficients.push_back(-1 * (a + b));
+  coefficients.push_back(a * b);
+  return coefficients;
+}
+
+// Best rated solution
+vector<int> quadratic_best(int a, int b) {
+  return { 1, - (a + b), a * b };
+}
