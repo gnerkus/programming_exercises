98111cf75701acf5fe38a1bd590901bfa6055e38
gnerkus <ifeanyioraelosi@gmail.com>
2021-08-09 21:32:39 +0200

    [SOLVED] Boolean to string

--- c/alan_patridge_II.c
+++ c/alan_patridge_II.c
@@ -45,3 +45,12 @@ char *apple(int x)
     return (x * x > 1000) ? "It's hotter than the sun!!" : "Help yourself to a honeycomb Yorkie for the glovebox.";
 }
 */
+
+/*
+Best practice: 0 Clever: 9
+
+char *apple(int x)
+{
+    return x > 31 ? "It's hotter than the sun!!" : "Help yourself to a honeycomb Yorkie for the glovebox.";
+}
+*/
---
+++ c/boolean_to_string.c
@@ -0,0 +1,40 @@
+/*
+Implement a function which convert the given boolean value into its string representation.
+*/
+
+#include <stdbool.h>
+#include <assert.h>
+#include <string.h>
+
+#include <stdio.h>
+
+//The returned string should be statically allocated and it won't be freed
+const char *boolean_to_string(bool b)
+{
+  return b ? "true" : "false";
+}
+
+int main() {
+  //assert(strncmp(boolean_to_string(true), "true", 4) == 0);
+  //assert(strncmp(boolean_to_string(false), "false", 5) == 0);
+}
+
+/*
+Best practice: 0 Clever: 6
+
+const char *boolean_to_string(_Bool b) {
+  return "false\0true" + b * 6;
+}
+
+Explanation:
+A string is a character array. Adding an int to a string moves the starting point
+of the character array. So:
+"false" + 0 // "false"
+"false" + 2 // "lse"
+"false" + 4 // "e"
+\0 is the termination of a string; any character after this is not printed
+*/
+
+/*
+
+*/
