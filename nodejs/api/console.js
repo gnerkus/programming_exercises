151e9d74d9181b99b2f42bb89026fa18454f96b3
gnerkus <ifeanyioraelosi@gmail.com>
2021-11-10 16:57:49 +0100

    Experiment with console.assert for custom logger

--- nodejs/api/console.js
+++ nodejs/api/console.js
@@ -7,12 +7,20 @@ console.error(new Error('test error log'));
 // Prints message to stderr
 console.warn('test warn log');
 
-const output = fs.createWriteStream(path.join(__dirname, '/stdout.log'));
-const errorOutput = fs.createWriteStream(path.join(__dirname, '/stderr.log'));
+const output = fs.createWriteStream(path.join(__dirname, '/stdout.log'), { flags: 'a' });
+const errorOutput = fs.createWriteStream(path.join(__dirname, '/stderr.log'), { flags: 'a' });
 // Custom simple logger
 const logger = new Console({ stdout: output, stderr: errorOutput });
 // use it like console
 const count = 5;
 logger.log('count: %d', count);
 // In stdout.log: count 5
+
+output.end();
+errorOutput.end();
+
+/**
+ * Assert
+ */
+logger.assert(false, 'Whoops %s work', 'did not');
 
