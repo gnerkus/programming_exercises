ca56d9655e6b4e5314e88a6b2b1ed08eb4938246
gnerkus <ifeanyioraelosi@gmail.com>
2022-12-22 00:20:17 +0100

    fix string reversal by using rear begin and rear end iterators

--- c++/exercism/reverse_string.cpp
+++ c++/exercism/reverse_string.cpp
@@ -3,10 +3,19 @@
 namespace reverse_string {
 std::string reverse(const std::string &source) {
   std::string reversed;
-  for (auto it = source.end(); it != source.begin(); --it) {
+  for (auto it = source.rbegin(); it != source.rend(); it++) {
     reversed += *it;
   }
 
   return reversed;
 }
 } // namespace reverse_string
+
+/**
+COMMUNITY SOLUTION
+
+inline std::string reverse_string(const std::string &src) {
+    return std::string(src.rbegin(), src.rend());
+}
+
+*/
